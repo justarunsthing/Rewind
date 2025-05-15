3f1c91447efe32085539aa2186f21dbc1f4ea62d
justarunsthing <justarunsthing@outlook.com>
2025-05-15 20:15:27 +0100

    Added script for Rewind app

--- js/site.js
+++ js/site.js
@@ -1,3 +1,26 @@
-function helloWorld() {
-    alert("Hello World!");
+function getString() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("user-string").value;
+    let reversedString = getReversedString(userString);
+
+    displayResult(reversedString);
+}
+
+function getReversedString(userString) {
+    let reversedString = [];
+
+    for (let i = userString.length - 1; i >= 0; i--){
+        reversedString += userString[i];
+    }
+
+    return reversedString;
+}
+
+function displayResult(reversedString){
+    // Write message on the page
+    document.getElementById("message").innerHTML = `The reversed string is: ${reversedString}`;
+
+    // Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
